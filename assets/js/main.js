09a2e84e903c7a617043a4b0cb81b09b001c3dc7
1-Koval4uk <46053952+1-Koval4uk@users.noreply.github.com>
2020-05-15 19:12:26 +0300

    Last version for desktop(without adaptive)

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,4 +21,15 @@ $('.nav-list__hamburger').click(function(){
 
 $('.left-side__close').click(function () {
     $('.left-side').addClass('non-active')
+});
+
+$(window).on('scroll', function(){
+    if($(window).scrollTop() >= 1000){
+        $('.btn-top').fadeIn();
+    }else{
+        $('.btn-top').fadeOut();
+    }
+})
+$('.btn-top').click(function(){
+    $('html, body').animate({scrollTop:0}, 800)
 })
